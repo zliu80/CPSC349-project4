1d9f699189cb737592d8a3069d5916ed8c6cf5a2
Jahnvi3100 <99162659+Jahnvi3100@users.noreply.github.com>
2022-12-16 17:07:37 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,8 @@ function About(){
     <a  href="#" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
 </div>
 <div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">xxxxxxx</h5>
-    <a  href="#" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jahnvi Panchal</h5>
+    <a  href="https://github.com/Jahnvi3100" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
 </div>
 <div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Zhiqiang Liu</h5>
