b12c30bbfee7c2c90b3c3dec6214b5a215d5c01c
Moses Merugu <m.merugu777@gmail.com>
2022-12-16 20:19:03 -0800

    Updating styling for About, added tailwind components

--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,22 @@ function About(){
     return (
         
 
-<div className="h-56 grid grid-cols-3 gap-4 content-around w-screen h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-<div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">xxxxxxx</h5>
-    <a  href="#" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
+<div className="grid grid-cols-4 w-screen h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+<div  className="about-card">
+    <h5 className="about-name">xxxxxxx</h5>
+    <a  href="#" className="about-gh">GitHub</a>
 </div>
-<div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Moses Merugu</h5>
-    <a  href="https://github.com/mmeru" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
+<div  className="about-card">
+    <h5 className="about-name">Moses Merugu</h5>
+    <a  href="https://github.com/mmeru" className="about-gh">GitHub</a>
 </div>
-<div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jahnvi Panchal</h5>
-    <a  href="https://github.com/Jahnvi3100" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
+<div  className="about-card">
+    <h5 className="about-name">Jahnvi Panchal</h5>
+    <a  href="https://github.com/Jahnvi3100" className="about-gh">GitHub</a>
 </div>
-<div  className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Zhiqiang Liu</h5>
-    <a  href="https://github.com/zliu80" className="font-normal text-gray-700 dark:text-gray-400">GitHub</a>
+<div  className="about-card">
+    <h5 className="about-name">Zhiqiang Liu</h5>
+    <a  href="https://github.com/zliu80" className="about-gh">GitHub</a>
 </div>
 </div>
     )
