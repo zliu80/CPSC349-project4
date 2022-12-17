13dcd8205a13c0d1e952c566f2e1aeb6636618ef
Moses Merugu <m.merugu777@gmail.com>
2022-12-17 15:30:10 -0800

    Menu shows sign in/sign out depending on status

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ async function refresh(){
 
 }
 
+console.log(pb.authStore.model)
 refresh()
 
 
@@ -136,15 +137,13 @@ render() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-
-                      
                       <Menu.Item>
                         {({ active }) => (
                           <p
-                            onClick={signout}
+                            onClick={pb.authStore.model == null ? active =>  window.location.href='/signin' : signout}
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Sign out
+                            {pb.authStore.model ==null ? 'Sign in' : 'Sign out'}
                           </p>
                         )}
                       </Menu.Item>
